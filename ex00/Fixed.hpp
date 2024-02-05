498b47abd6b239af3d7adaf585871e94094feba0
Jorge Bortolotti Suarez <jbortolo@car1s3.42barcelona.com>
2024-02-05 18:01:24 +0100

    empezado el segundo ejercicio

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -11,9 +11,14 @@ private:
 public:
 	Fixed();
 	Fixed(const Fixed &fixed);
+	Fixed(const int num);
+	Fixed(const float num);
 	~Fixed();
 	void setRawBits(const int bit);
 	int getRawBits(void) const;
+	int	toInt(void) const;
+	float toFloat(void) const;
+	Fixed &operator<<(const Fixed &fi);
 	Fixed &operator=(const Fixed &fi);
 };
 
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	/* data */
+public:
+	Fixed(/* args */);
+	~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
+
+
+#endif
